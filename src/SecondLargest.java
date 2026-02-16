a387ff1d197c3cfc7a1d4da1e2cabbff8594dcbb
SREEVINAYAKG <srivinayakg2005@gmail.com>
2026-02-16 21:53:34 +0530

    Submitted secondLargest

--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
 public class SecondLargest {
-    
-        // TODO: Read N
-        // TODO: Read array elements
-        // TODO: Find and print the second largest element
-        // Hint: You can do this in a single pass or two passes without sorting.
+        
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+                int[] arr = new int[n];
+                for (int i = 0; i < n; i++) {
+                        arr[i] = sc.nextInt();
+                }
+                int fLargest = Integer.MIN_VALUE;
+                int sLargest = Integer.MIN_VALUE;
+                for(int i=0;i<n;i++){
+                        if(arr[i]>fLargest){
+                                sLargest = fLargest;
+                                fLargest = arr[i];
+                        }else if(arr[i]>sLargest && arr[i]!=fLargest){
+                                sLargest = arr[i];
+                        }
+                }
+                if(n<2) System.out.println(-1);
+                else System.out.println(sLargest);
+        }
     
 }
